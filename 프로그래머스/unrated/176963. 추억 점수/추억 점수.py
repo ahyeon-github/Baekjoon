85f86d022aa402c265bad907e102cd52ca0616e7
Ahyeon Lim <80513699+ahyeon-github@users.noreply.github.com>
2023-08-22 14:41:40 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,15 @@
+# default = 0
+from collections import defaultdict
+def solution(name, yearning, photo):
+    answer = []
+    sH = defaultdict(int)
+    
+    for i in range(len(name)):
+        sH[name[i]] = yearning[i]
+    for i in photo:
+        sum = 0
+        for j in i:
+            sum += sH[j]
+        answer.append(sum)
+                   
+    return answer
