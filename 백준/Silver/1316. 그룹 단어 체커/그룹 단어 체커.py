63c63f241bcb29cb8c2d398518ce05d0ad411fe7
Ahyeon Lim <80513699+ahyeon-github@users.noreply.github.com>
2023-11-21 20:59:42 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,21 +1,17 @@
-n = int(input())
-#count = 0
-count = n
-for i in range(n):
+word_count =int(input())
+count = word_count
+for i in range(word_count):
     word = input()
-    # 단어들을 하나씩 보면서
-    # 그룹단어인지 체크한다
-    # 단어들을 하나씩 보고 원소들 중에 뒤에 중복된 원소가 나오면 그룹단어가 아님
-    # 단어 각 원소들 바로 이어지는 것이면 그룹체커 , 단어들이 띄어져 있으면 그룹 체커가 아님
     for j in range(0, len(word)-1):
-        if word [j] == word[j+1]:
+        if word[j] == word[j+1]:
             pass
-        
-        elif word[j] in word[j+2:]:
-            count-=1
+        elif word[j] in word[j+1:]:
+            count -= 1
             break
-print(count)
         
+print(count) 
+'''
+단어들이 계속 연속하여 등장하는지 확인을 하기 위해 자료구조 스택을 사용
+각 문자들이 들어올 때 스택에 넣어준다. 
 
-
-
+'''
