a02cb078194cc8248b77778e32a43d78cee03175
Ahyeon Lim <80513699+ahyeon-github@users.noreply.github.com>
2023-12-24 05:28:26 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 52 ms, Memory: 31132 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -1,23 +1,22 @@
-import sys; input = sys.stdin.readline
+import sys
 
-def dfs(i, j): # DFS
-    result = 0
-    for ii, jj in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]: # 상하좌우
-        # 다음 위치가 범위 안이며 방문하지 않은 위치이며 병사의 종류가 시작 위치와 같아야 한다.
-        if 0 <= ii < M and 0 <= jj < N and not visited[ii][jj] and matrix[m][n] == matrix[ii][jj]:
-            visited[ii][jj] = True # 방문 체크
-            result += 1 # 결과에 1 더하고
-            result += dfs(ii, jj) # 다음 위치 DFS
-    return result # 결과 반환
+def dfs(y, x, cnt):
+    c = graph[y][x]
+    graph[y][x] = 1
+    for dy, dx in d:
+        Y, X = y+dy, x+dx
+        if (0 <= Y < M) and (0 <= X < N) and graph[Y][X] == c:
+            cnt = dfs(Y, X, cnt+1)
+    return cnt
 
 N, M = map(int, input().split())
-matrix = [input().strip() for _ in range(M + 1)]
-visited = [[False] * N for _ in range(M + 1)] # 방문 체크할 행렬도 전쟁터의 크기와 똑같이 만든다.
-W = B = 0
-for m in range(M):
-    for n in range(N):
-        if not visited[m][n]: # 만약 방문하지 않은 위치라면
-            visited[m][n] = True # 방문 체크 후
-            result = 1 + dfs(m, n) # 그 위치의 1명 + DFS 결과값이 뭉쳐있는 병사 수가 된다.
-            exec('%s += result ** 2' % matrix[m][n]) # 병사들이 속해있는 국가의 위력에 뭉쳐있는 병사 수의 제곱만큼 더해준다.
-print(W, B) # 정답 출력
+graph = [list(input()) for _ in range(M)]
+d = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+w = b = 0
+for i in range(M):
+    for j in range(N):
+        if graph[i][j] == 'W':
+            w += dfs(i, j, 1)**2
+        elif graph[i][j] == 'B':
+            b += dfs(i, j, 1)**2
+print(w, b)
